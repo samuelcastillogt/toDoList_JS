114439bffd18cec6c4bc4e2432983e839ba59c7e
Samuel Castillo <73082164+samuelcastillogt@users.noreply.github.com>
2022-01-31 20:42:30 -0600

    Correcciones de la logica y efectos en CSS

--- to-do.js
+++ to-do.js
@@ -1,10 +1,40 @@
 const guardar = document.getElementById("guardar")
 const cardCollection = document.getElementById("card-collection")
+const input = document.getElementById("input")
 const form = document.getElementById("form")
 const crear = document.getElementById("crear")
+const blure = document.getElementById("blur")
+const tasks = []
+input.addEventListener("input", ()=>{
+    if(tasks.length >= 1){
+    const filtro = tasks.filter(item=> item.includes(input.value))
+    inyector(filtro)
+    }else{
+        
+    }
+    
+})
+
+crear.addEventListener("click", ()=>{
+    form.classList.remove("hidden")
+    blure.classList.add("blur")
+    crear.classList.add("hidden")
+})
+const inyector =(tasks)=>{
+        cardCollection.innerHTML=""    
+        tasks.map(item=> {
+            const card = document.createElement("li")
+            card.textContent = item
+            card.appendChild(addRemoveBtn())
+            cardCollection.appendChild(card) 
+        })
+}
+   
+    //
+
 function addRemoveBtn(){
     const removeBtn = document.createElement("button")
-    removeBtn.textContent= "Terminada"
+    removeBtn.textContent= `x`
     removeBtn.className= "btn btn-danger"
     removeBtn.addEventListener("click", (e)=>{
         const task = e.target.parentElement
@@ -13,10 +43,8 @@ function addRemoveBtn(){
     return removeBtn
 }
 const task = (title)=>{
-    const card = document.createElement("li")
-    card.textContent = title
-    card.appendChild(addRemoveBtn())
-    cardCollection.appendChild(card) 
+    tasks.push(title)
+    inyector(tasks)
 }
 const save = (e)=>{
     e.preventDefault()
@@ -24,10 +52,8 @@ const save = (e)=>{
     task(title)
     form.reset()
     form.classList.add("hidden")
+    blure.classList.remove("blur")
     crear.classList.remove("hidden")
+
 }
-crear.addEventListener("click", ()=>{
-    form.classList.remove("hidden")
-    crear.classList.add("hidden")
-})
 guardar.addEventListener("click", save)
